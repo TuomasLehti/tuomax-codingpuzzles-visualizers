af632b14891cc13f34c935444fbdec11a136d1a7
Tuomas Lehti <tuomas.a.lehti@gmail.com>
2025-11-16 16:00:30 +0200

    Adds input from user

--- everybodycodes/year2024/quest08/sketch.js
+++ everybodycodes/year2024/quest08/sketch.js
@@ -1,4 +1,4 @@
-const NUM_OF_NAILS = 8;
+const NUM_OF_NAILS = 256;
 
 const ARTWORK_DIAMETER = 300;
 
@@ -6,47 +6,45 @@ const ARTWORK_RADIUS = ARTWORK_DIAMETER / 2;
 
 function setup() {
   createCanvas(400, 400);
-  frameRate(16);
-  translate(width / 2, height / 2);
+//  frameRate(16);
+  resetSketch();
+//  translate(width / 2, height / 2);
 //  drawNails();
 }
 
-let fromIdx = 0;
+let nails;
+let nailIdx = 0;
 
-let toIdx = 1;
+function resetSketch()
+{
+  background(255);
+  parseInput();
+  nailIdx = 0;
+}
+
 
 function draw() {
   translate(width / 2, height / 2);
-  drawSpan(fromIdx, toIdx);
-  toIdx++;
-  if (toIdx == NUM_OF_NAILS) {
-    fromIdx++;
-    if (fromIdx == NUM_OF_NAILS) {
-      noLoop();
-    }
-    toIdx = fromIdx + 1;
+  drawSpan(nails[nailIdx], nails[nailIdx + 1]);
+  nailIdx++;
+  if (nailIdx == nails.length - 1) {
+    noLoop();
+    console.log("Done.");
   }
-}
-
-function drawNails() {
-  for (let idx = 0; idx < NUM_OF_NAILS; idx++)
-      drawNail(idx);
-}
-
-function drawNail(idx) {
-  let coords = nailIdxToCoords(idx);
-  fill(0);
-  stroke(0);
-  strokeWeight(2);
-  circle(coords.x, coords.y, 10);
+  drawSpan(nails[nailIdx], nails[nailIdx + 1]);
+  nailIdx++;
+  if (nailIdx == nails.length - 1) {
+    noLoop();
+    console.log("Done.");
+  }
 }
 
 function drawSpan(fromNailIdx, toNailIdx) {
   let fromCoords = nailIdxToCoords(fromNailIdx);
   let toCoords = nailIdxToCoords(toNailIdx);
-  stroke(0, 0, 128);
-  fill(0, 0, 128);
-  strokeWeight(2);
+  stroke(0, 0, 128, 24);
+  fill(0, 0, 128, 24);
+  strokeWeight(1);
   line(fromCoords.x, fromCoords.y, toCoords.x, toCoords.y);
 }
 
@@ -57,3 +55,11 @@ function nailIdxToCoords(nailIdx) {
   let y = ARTWORK_RADIUS * sin(angleRad);
   return createVector(x, y);
 }
+
+function parseInput() {
+  nails = document
+    .getElementById("ecinput").value
+    .split(",").map(str => Number(str));
+  return nails;
+}
+
